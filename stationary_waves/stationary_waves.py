51d04b2812a311b39c7364dbaf8cf3ee6fbfad88
hachispin <ganalonrainier255@gmail.com>
2025-12-16 21:54:30 +0000

    Update L -> 1/L
    this is what it should be for a linear relationship

--- stationary_waves/stationary_waves.py
+++ stationary_waves/stationary_waves.py
@@ -4,7 +4,7 @@ Required Practical 1: "Investigating Stationary Waves".
 Code for investigating graphical relation of the
 frequency of the first harmonic against length.
 
-Lab report due December 17th, 2025. for Mr. Awuah.
+Lab report due December 17th, 2025 for Mr. Awuah.
 """
 
 import numpy as np
@@ -13,7 +13,8 @@ import matplotlib.pyplot as plt
 from numpy.polynomial import Polynomial
 
 # The data gathered and recorded in my lab book.
-POINTS = (
+# Note that L hasn't been adjusted to be 1/L here yet.
+DATA = (
     # (length, frequency)
     (0.3, 250.0),
     (0.4, 200.0),
@@ -23,25 +24,33 @@ POINTS = (
     (0.8, 83.33),
 )
 
+# Adjust L -> 1/L for plotting.
+points = tuple((1 / d[0], d[1]) for d in DATA)
+
 # Unpack `POINTS` to x, y arrays.
-x_values, y_values = map(np.array, zip(*POINTS))
-print(f"Plotting points: {POINTS}")
+reciprocal_length, frequency = map(np.array, zip(*points))
+print(f"Plotting points: {points}")
 
 # Find the degree 1 (linear) LOBF's coefficients.
-lobf_c, lobf_m = Polynomial.fit(x_values, y_values, deg=1).convert().coef
+poly_lobf = Polynomial.fit(reciprocal_length, frequency, deg=1)
+lobf_c, lobf_m = poly_lobf.convert().coef
+
 print(f"LOBF gradient (m): {lobf_m}")
 print(f"LOBF y-intercept (c): {lobf_c}")
 print(f"LOBF equation: y = {lobf_m:.3f}x + {lobf_c:.3f}")
 
-# Extend the LOBF by `MARGIN`.
+# Extend the LOBF by `MARGIN` (stylistic choice).
 #
 # I've done this so the points (marked with crosses)
 # don't get cut off when margins are set to zero.
 MARGIN = 0.05
-lobf_x_values = np.array((min(x_values) - MARGIN, max(x_values) + MARGIN))
+lobf_x = np.array((
+    min(reciprocal_length) - MARGIN,
+    max(reciprocal_length) + MARGIN
+))
 
 # Linear equation: y = mx + c
-lobf = lobf_m * lobf_x_values + lobf_c
+lobf_y = lobf_m * lobf_x + lobf_c
 
 # Setup plotting
 A4_SIZE = (11.69, 8.27)  # (x, y)
@@ -49,13 +58,15 @@ FIGURE_NAME = "Stationary_Waves.png"
 plt.figure(figsize=A4_SIZE)
 
 # Set appropriate labels
-plt.title("Investigating how the frequency of the first harmonic changes with length")
-plt.xlabel("Length (m)")
-plt.ylabel("Frequency (Hz)")
+plt.xlabel(r"Length, 1/L $(m^{-1})$")
+plt.ylabel("Frequency, f (Hz)")
+plt.title(
+    "Investigating how the frequency of the first harmonic changes with length"
+)
 
-# Plot `POINTS` and `lobf`.
-plt.scatter(x_values, y_values, marker="x", color="Black")
-plt.plot(lobf_x_values, lobf)
+# Plot `points` and `lobf`.
+plt.scatter(reciprocal_length, frequency, marker="x", color="Black")
+plt.plot(lobf_x, lobf_y)
 
 plt.grid()  # Set grid (stylistic choice).
 plt.margins(x=0)  # Removing margins as per Mr. Awuah.
@@ -63,8 +74,8 @@ plt.savefig(FIGURE_NAME)
 
 # Expected output:
 #
-# Plotting points: ((0.3, 250.0), (0.4, 200.0), (0.5, 166.7), (0.6, 125.0), (0.7, 111.1), (0.8, 83.33))
-# LOBF gradient (m): -326.2142857142857
-# LOBF y-intercept (c): 335.43952380952385
-# LOBF equation: y = -326.214x + 335.440
+# Plotting points: ((3.3333333333333335, 250.0), (2.5, 200.0), (2.0, 166.7), (1.6666666666666667, 125.0), (1.4285714285714286, 111.1), (1.25, 83.33))
+# LOBF gradient (m): 78.68128660180521
+# LOBF y-intercept (c): -3.6826114953308036
+# LOBF equation: y = 78.681x + -3.683
 #
